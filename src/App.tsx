b31c80615ad8114c59e8ad73849edce04aa856cf
Dima Shuhayeu <shuga2704@gmail.com>
2023-07-02 13:13:51 +0300

    Finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
-import React, { useState, useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import './App.css'
 import BoardComponent from './components/BoardComponent'
 import { Board } from './models/Board'
+import { Player } from './models/Player'
+import { Colors } from './models/Colors'
 
 function App() {
   const [board, setBoard] = useState(new Board())
+  const [whitePlayer, setWhitePlayer] = useState(new Player(Colors.WHITE))
+  const [blackPlayer, setBlackPlayer] = useState(new Player(Colors.BLACK))
+  const [currentPlayer, setCurrentPlayer] = useState<Player | null>(null)
 
   const restart = () => {
     const newBoard = new Board()
@@ -13,13 +18,25 @@ function App() {
     setBoard(newBoard)
   }
 
+  const swapPlayer = () => {
+    setCurrentPlayer(
+      currentPlayer?.color === Colors.WHITE ? blackPlayer : whitePlayer,
+    )
+  }
+
   useEffect(() => {
     restart()
+    setCurrentPlayer(whitePlayer)
   }, [])
 
   return (
     <div className="app">
-      <BoardComponent board={board} setBoard={setBoard} />
+      <BoardComponent
+        board={board}
+        setBoard={setBoard}
+        currentPlayer={currentPlayer}
+        swapPlayer={swapPlayer}
+      />
     </div>
   )
 }
--- src/components/BoardComponent.tsx
+++ src/components/BoardComponent.tsx
@@ -1,38 +1,78 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Board } from '../models/Board'
 import CellComponent from './CellComponent'
 import { Cell } from '../models/Cell'
+import { Player } from '../models/Player'
+import { Colors } from '../models/Colors'
 
 interface IBoardProps {
   board: Board
   setBoard: (board: Board) => void
+  currentPlayer: Player | null
+  swapPlayer: () => void
 }
 
-const BoardComponent: React.FC<IBoardProps> = ({ board, setBoard }) => {
+const BoardComponent: React.FC<IBoardProps> = ({
+  board,
+  setBoard,
+  currentPlayer,
+  swapPlayer,
+}) => {
   const [selectedCell, setSelectedCell] = useState<Cell | null>(null)
 
+  useEffect(() => {
+    highlightCells()
+  }, [selectedCell])
+
   function click(cell: Cell) {
-    if (cell.figure) {
-      setSelectedCell(cell)
+    if (
+      selectedCell &&
+      selectedCell !== cell &&
+      selectedCell.figure?.canMove(cell)
+    ) {
+      selectedCell.moveFigure(cell)
+      swapPlayer()
+      setSelectedCell(null)
+      updateBoard()
+    } else {
+      if (cell.figure?.color === currentPlayer?.color) {
+        setSelectedCell(cell)
+      }
     }
   }
 
+  function highlightCells() {
+    board.highlightCells(selectedCell)
+    updateBoard()
+  }
+
+  function updateBoard() {
+    const newBoard = board.getCopyBoard()
+    setBoard(newBoard)
+  }
+
   return (
-    <div className="board">
-      {board.cells.map((row, index) => (
-        <React.Fragment key={index}>
-          {row.map(cell => (
-            <CellComponent
-              key={cell.id}
-              cell={cell}
-              selected={
-                cell.x === selectedCell?.x && cell.y === selectedCell?.y
-              }
-              click={click}
-            />
-          ))}
-        </React.Fragment>
-      ))}
+    <div>
+      <h3>
+        Ход игрока: {currentPlayer?.color === Colors.WHITE ? 'Белые' : 'Черные'}
+      </h3>
+
+      <div className="board">
+        {board.cells.map((row, index) => (
+          <React.Fragment key={index}>
+            {row.map(cell => (
+              <CellComponent
+                key={cell.id}
+                cell={cell}
+                selected={
+                  cell.x === selectedCell?.x && cell.y === selectedCell?.y
+                }
+                click={click}
+              />
+            ))}
+          </React.Fragment>
+        ))}
+      </div>
     </div>
   )
 }
